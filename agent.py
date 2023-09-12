f2ceb01bd8de97976a31baec81126c0fbca13bd0
gardenjun <jeongwonjun0718@gmail.com>
2023-09-12 14:54:47 +0900

    fix : Agent class Comments

--- agent.py
+++ agent.py
@@ -19,7 +19,7 @@ class Agent:
     NUM_ACTIONS = len(ACTIONS) # 인공 신경망에서 고려할 출력값의 개수
 
     def _init_(self, 
-               environment, 
+               environment,  # Environment 클래스의 객체
                min_trading_unit = 1, # 최소 매매 단위
                max_trading_unit = 2, # 최대 매매 단위
                delayed_reward_threshold = 0.05 # 지연보상 임계치
@@ -39,15 +39,15 @@ class Agent:
         self.balance = 0 # 현재 현금 잔고
         self.num_stocks = 0 # 보유 주식 수
         # PV = balance + num_stocks * {현재 주식 가격}
-        self.portfolio_value = 0
-        self.base_portfolio_value = 0 # 직접 학습 시점의 PV
+        self.portfolio_value = 0 # 보유 포트폴리오 가치 = 보유현금 + 보유 주식 수 * 현재 주가
+        self.base_portfolio_value = 0 # 직접 학습 시점의 PV # 목표수익률 또는 기준 손실률을 달성하기 전의 과거 포트폴리오 가치
         self.num_buy = 0 # 매수 횟수
         self.num_sell = 0 # 매도 횟수
         self.num_hold = 0 # 홀딩 횟수
-        self.immediate_reward = 0 # 즉시 보상
+        self.immediate_reward = 0 # 에이전트가 가장 최근 행한 행동에 대한 즉시 보상 값
         self.profitloss = 0 # 현재 손익
         self.base_profitloss = 0 # 직전 지연 보상 이후 손익
-        self.exploration_base = 0 # 탐험 행동 결과 기준
+        self.exploration_base = 0 # 탐험 행동 결과 기준 (매수 또는 매도 기준 정하기) # 탐험 기준 확률
 
         # Agent 클래스의 상태
         self.ratio_hold = 0 # 주식 보유 비율
