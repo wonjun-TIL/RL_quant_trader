ac42ca35be6d9ea07f3415b7e94999d66f9a0ae7
gardenjun <jeongwonjun0718@gmail.com>
2023-09-20 22:43:46 +0900

    feat : network 생성자 함수

--- networks.py
+++ networks.py
@@ -26,3 +26,16 @@ elif os.environ['KERAS_BACKEND'] == 'plaidml.keras.backend':
     from keras.optimizers import SGD
 
 
+class Network:
+    lock = threading.Lock() # 스레드를 이용해 병렬로 신경망을 사용하기 때문에 스레드 간의 충돌 방지를 위해 lock 클래스의 객체를 생성
+
+    def __init__(self, input_dim=0, output_dim=0, lr=0.001, shared_network=None, activation='sigmoid', loss='mse'):
+        self.input_dim = input_dim  # 입력 데이터의 크기
+        self.output_dim = output_dim # 출력 데이터의 크기
+        self.lr = lr    # 학습률
+        self.shared_network = shared_network # 공유 신경망
+        self.activation = activation # 활성화 함수
+        self.loss = loss # 손실 함수
+        self.model = None # 신경망 모델
+
+    
