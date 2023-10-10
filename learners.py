3f430bf9bb0facb15ae1064cccc1d1cc50220217
garden-jun <jeongwonjun0718@gmail.com>
2023-10-10 22:30:42 +0900

    feat : Add 강화학습 실행(run) 함수

--- learners.py
+++ learners.py
@@ -215,8 +215,195 @@ class ReinforcementLearner:
         # 배치 학습 데이터 생성 및 신경망 갱신
         if self.batch_size > 0:
             _loss = self.update_networks(self.batch_size, delayed_reward, discount_factor)
+            # 나중에 loss를 learning_cnt로 나누어 에포크의 학습 손실로 여김
             if _loss is not None:
                 self.loss += abs(_loss)
                 self.learning_cnt += 1
                 self.memory_learning_idx.append(self.training_data_idx)
             self.batch_size = 0
+
+    
+    def run(self, 
+            num_epoches=100, # 수행할 반복 학습 횟수
+            balance = 10000000, # 초기 투자 자본금
+            discount_factor = 0.9, # 상태-행동 가치를 구할 때 적용할 할인율
+                                    # 보상이 발생했을 때 그 이전 보상이 발생한 시점과 현재 보상이 발생한 시점 사이에서
+                                    # 수행한 행동 전체에 현재의 보상이 영향을 미침
+                                    # 과거로 갈 수록 현재의 보상을 약하게 적용
+            start_epsilon=0.5,  # 초기 탐험 비율. 전혀 학습되지 않은 초기에는 탐험 비율을 높여 무작위 투자를 수행. 
+                                # 탐험을 통해 특정 상황에서 좋은 행동과 그렇지 않은 행동을 결정하기 위한 경험을 쌓음.
+            learning=True   # 학습을 해서 신경망 모델을 만들고자 한다면 learning=True, 
+                            # 학습된 모델을 가지고 투자 시뮬레이션만 하려한다면 learning=False
+            ):
+        info = "[{code}] RL:{rl} Net:{net} LR:{lr}" \
+            "DF:{discount_factor} TU:[{min_trading_unit}," \
+                "{max_trading_unit}] DRT:{delayed_reward_threshold}".format(
+            code=self.stock_code, rl=self.rl_method, net=self.net,
+            lr=self.lr, discount_factor=discount_factor,
+            min_trading_unit=self.agent.min_trading_unit,
+            max_trading_unit=self.agent.max_trading_unit,
+            delayed_reward_threshold=self.agent.delayed_reward_threshold
+        )
+
+        with self.lock:
+            logging.info(info)
+
+        # 시작 시간
+        time_start = time.time()
+
+
+        '''가시화
+        # 가시화 준비
+        # 차트 데이터는 변하지 않으므로 미리 가시화
+        self.visualizer.prepare(self.environment.chart_data, info)
+
+        # 가시화 결과 저장할 폴더 준비
+        self.epoch_summary_dir = os.path.join(self.output_path, f'epoch_summary_{self.stock_code}')
+        if not os.path.isdir(self.epoch_summary_dir):
+            os.makedirs(self.epoch_summary_dir)
+        else:
+            for f in os.listdir(self.epoch_summary_dir):
+                os.remove(os.path.join(self.epoch_summary_dir, f))
+
+        '''
+
+        # 에이전트 초기 자본금 설정
+        self.agent.set_balance(balance)
+        
+        # 학습에 대한 정보 초기화
+        max_portfolio_value = 0 # 수행한 에포크 중에서 가장 높은 포트폴리오 가치
+        epoch_win_cnt = 0   # 수행한 에포크 중에서 수익이 발생한 에포크 수 (포트폴리오 가치가 초기 자본금 보다 높아진 에포크 수)
+
+        # 에포크 반복
+        for epoch in range(num_epoches):
+            time_start_epoch = time.time()  # 한 에포크를 수행하는데 필요한 시간 기록
+
+            # step 샘플을 만들기 위한 큐
+            q_sample = collections.deque(maxlen=self.num_steps)
+            
+            # 환경, 에이전트, 신경망, 가시화, 메모리 초기화
+            self.reset()
+
+            # 학습을 진행할 수록 탐험 비율 감소
+            if learning:
+                epsilon = self.start_epsilon * (1. - float(epoch) / (num_epoches - 1))
+                self.agent.reset_exploration()
+            else:
+                epsilon = self.start_epsilon
+            
+            while True:
+                # 샘플 생성
+                next_sample = self.build_sample() # 환경 객체로부터 하나의 샘플을 읽어옴
+                if next_sample is None: # 마지막까지 다 읽은것이므로 break
+                    break
+
+                # num_steps만큼 샘플 저장 # 다 준비돼야 행동을 결정할 수 있음.
+                q_sample.append(next_sample)   
+                if len(q_sample) < self.num_steps:  # 다 안찼으면 로직을 건너뜀
+                    continue
+
+                # 가치, 정책 신경망 예측
+                pred_value = None
+                pred_policy = None
+                # 각 신경망의 predict함수를 호출해 예측 행동 가치와 예측행동 확률을 구함
+                if self.value_network is not None:
+                    pred_value = self.value_network.predict(list(q_sample)) 
+                if self.policy_network is not None:
+                    pred_policy = self.policy_network.predict(list(q_sample))
+                
+                # 신경망 또는 탐험에 의한 행동 결정
+                # 무작위 투자 비율인 epsiolon 값의 확률로 무작위로 하거나, 
+                # 신경망의 출력을 통해 결정. 
+                # 정책 신경망의 출력은 매수를 했을 때와 매도를 했을 때의 포트폴리오 가치를 높일확률을 의미
+                # 즉, 매수에 대한 정책 신경망 출력이 매도에 대한 출력보다 높으면 매수, 그 반대면 매도
+                # 정책 신경망의 출력이 없으면 가치 신경망의 출력값이 높은 행동을 선택
+                # 가치 신경망의 출력은 행동에 대한 예측가치(손익률)를 의미.
+
+                # 결정한 행동인 action, 결정에 대한 확신도인 confidence, 무작위 투자 유무인 exploration
+                action, confidence, exploration = self.agent.decide_action(pred_value, pred_policy, epsilon)
+
+                # 결정한 행동을  수행하고 즉시 보상과 지연 보상 획득
+                immediate_reward, delayed_reward = self.agent.act(action, confidence)
+
+                # 행동 및 행동에 대한 결과를 기억
+                self.memory_sample.append(list(q_sample))   # 학습 데이터의 샘플
+                self.memory_action.append(action)   # 에이전트 행동
+                self.memory_reward.append(immediate_reward) # 즉시보상
+                if self.value_network is not None:
+                    self.memory_value.append(pred_value) # 가치 신경망 출력
+                if self.policy_network is not None:
+                    self.memory_policy.append(pred_policy) # 정책 신경망 출력
+                self.memory_pv.append(self.agent.portfolio_value) # 포트폴리오 가치
+                self.memory_num_stocks.append(self.agent.num_stocks) # 보유 주식 수
+                if exploration:
+                    self.memory_exp_idx.append(self.training_data_idx) # 탐험 위치를 저장
+
+                # 반복에 대한 정보 갱신
+                self.batch_size += 1 # 배치크기
+                self.itr_cnt += 1 # 반복 카운팅 횟수
+                self.exploration_cnt += 1 if exploration else 0 # 탐험한 경우에만 1을 증가
+
+                # 지연보상이 발생된 경우 미니 배치 학습
+                # 지연보상은 지연보상임계치가 넘는 손익률이 발생했을 때 주어짐
+                if learning and (delayed_reward != 0): 
+                    self.fit(delayed_reward, discount_factor)
+
+            # 에포크 종료 후 학습
+            if learning:    # 남은 미니배치 학습
+                self.fit(self.agent.profitloss, discount_factor)
+
+            # 에포크 관련 정보 로그 기록
+            num_epoches_digit = len(str(num_epoches)) # 1000번이면 0001 부터 에포크를 시작하기 위해
+            epoch_str = str(epoch + 1).rjust(num_epoches_digit, '0')
+            time_end_epoch = time.time()
+            elapsed_time_epoch = time_end_epoch - time_start_epoch # 한 에포크를 수행하는데 걸린 시간
+            
+            # loss는 에포크 동안 수행한 미니배치들의 학습 손실을 모두 더해놓은 상태
+            # loss를 학습 횟수만큼 나눠서 미니배치의 평균 학습손실로 갱신
+            if self.learning_cnt > 0:
+                self.loss /= self.learning_cnt 
+
+            logging.info("[{}][Epoch {}/{}] Epsilon:{:.4f} "
+                         "#Expl.:{}/{} #Buy:{} #Sell:{} #Hold:{} "
+                         "#Stocks:{} PV:{:, .0f} "
+                         "LC:{} Loss:{:.6f} ET:{:.4f}".format(
+                             self.stock_code, # 주식 종목 코드
+                             epoch_str, # 현재 에포크 번호
+                             num_epoches, #
+                             epsilon, # 해당 에포크에서의 탐험률
+                             self.exploration_cnt, # 에포크 동안 수행한 탐험 횟수
+                             self.itr_cnt,  # 에포크 동안 수행한 행동 횟수
+                             self.agent.num_buy, # 에포크 동안 수행한 매수 횟수
+                             self.agent.num_sell, # 에포크 동안 수행한 매도 횟수
+                             self.agent.num_hold, # 에포크 동안 수행한 홀드 횟수
+                             self.agent.num_stocks, # 에포크 종료 시점에 보유하고 있는 주식 수
+                             self.agent.portfolio_value, # 에포크 종료 시점에 포트폴리오 가치
+                             self.learning_cnt, # 에포크 동안 수행한 미니배치 학습 횟수
+                             self.loss, # 에포크 동안 수행한 미니배치 학습 손실
+                             elapsed_time_epoch # 에포크 수행 시간
+                             ))
+
+
+            # 에포크 관련 정보 가시화
+            # self.visualize(epoch_str, num_epoches, epsilon)
+
+            # 학습 관련 정보 갱신
+            # 최대 포트폴리오 가치를 갱신하고 해당 에포크에서 포트폴리오 가치가 자본금보다 높으면 epoch_win_cnt를 1 증가
+            max_portfolio_value = max(
+                max_portfolio_value, self.agent.portfolio_value)
+            if self.agent.portfolio_value > self.agent.initial_balance:
+                epoch_win_cnt += 1
+
+        # 종료 시간
+        time_end = time.time()
+        elapsed_time = time_end - time_start
+
+        # 학습 관련 정보 로그 기록
+        with self.lock:
+            logging.info("[{code}] Elapsed Time:{elapsed_time:.4f} "
+                         "Max PV:{max_pv:,.0f} #Win:{cnt_win}".format(
+                             code=self.stock_code,
+                             elapsed_time=elapsed_time,
+                             max_pv=max_portfolio_value,
+                             cnt_win=epoch_win_cnt
+                         ))
